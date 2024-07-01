b4f69bd3f7fac2a03904bc1d9bf30a42f6d70701
MattTJung <124365945+MattTJung@users.noreply.github.com>
2024-07-01 17:52:31 +0200

    publish index html with list of all resources

---
+++ .github/workflows/index.html.py
@@ -0,0 +1,47 @@
+import argparse
+
+def gen_html(writer, **kwargs):
+    writer.write('<!DOCTYPE html>\n')
+    writer.write('<html>\n')
+    writer.write(f'<title>{title:s}</title>\n')
+    writer.write('<body>\n')
+    writer.write(f'<h1>{title:s}</h1>\n')
+    writer.write('<h2>RDF resources</h2>\n')
+    writer.write('<ul>\n')
+    writer.write(f'<li><a href="https://{repo_owner:s}.github.io/{repo_name:s}/{rdfname:s}.ttl">https://{repo_owner:s}.github.io/{repo_name:s}/{rdfname:s}.ttl</a></li>')
+    writer.write(f'<li><a href="https://{repo_owner:s}.github.io/{repo_name:s}/{rdfname:s}.rdf">https://{repo_owner:s}.github.io/{repo_name:s}/{rdfname:s}.rdf</a></li>')
+    writer.write('</ul>\n')
+    writer.write('<h2>File resources</h2>\n')
+    writer.write('<ul>\n')
+    for resource in resources:
+        writer.write(f'<li><a href="https://{repo_owner:s}.github.io/{repo_name:s}/{resource:s}">https://{repo_owner:s}.github.io/{repo_name:s}/{resource:s}</a></li>')
+    writer.write('</ul>\n')
+    writer.write('</body>\n')
+    writer.write('<html>\n')
+
+def make_parser():
+    parser_ = argparse.ArgumentParser()
+    parser_.add_argument('-t', '--title', required=True, help='Title for the index page')
+    parser_.add_argument('-o', '--owner', required=True, help='Repo owner/group')
+    parser_.add_argument('-n', '--name', required=True, help='Repo name')
+    parser_.add_argument('-r', '--rdfname', required=True, help='Name of the rdf files (without extension)')
+    parser_.add_argument('-l', '--resourceslist', required=True, help='List of all resources (with extension)')
+    parser_.add_argument('-h', '--html', required=True, help='Path of the target html file')
+
+if __name__ == '__main__':
+    parser = make_parser()
+    args = parser.parse_args()
+
+    with open(args.resourceslist, 'r', encoding='utf8') as f:
+        resources_ = [line.strip() for line in f]
+
+    html_args = {
+        "title": args.title,
+        "repo_owner": args.owner,
+        "repo_name": args.name,
+        "rdfname": args.rdfname,
+        "resources": resources_
+    }
+
+    with open(args.html, 'w', encoding='utf8') as f:
+        gen_html(f, **html_args)
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # pmd_referenceproject
+
+This repository contains data from a joint project between Fraunhofer IWM, Freiburg and IWT, Bremen. The provided python scripts generate a structured dataset using the [PMDco v2.0.7](https://github.com/materialdigital/core-ontology) and [TTO v2.0.1](https://github.com/materialdigital/application-ontologies). The resulting ABox is published to [github-pages](https://matttjung.github.io/pmd_referenceproject/) as .ttl and .rdf alongside all references file resources.
+
+**The repo is currently WIP**
