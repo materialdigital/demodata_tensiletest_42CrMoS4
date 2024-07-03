0a45f1fc2df5407f57628d8304aa61a562c13f54
MattTJung <124365945+MattTJung@users.noreply.github.com>
2024-07-03 12:04:58 +0000

    fix index.html and .htaccess

--- .github/workflows/index.html.py
+++ .github/workflows/index.html.py
@@ -1,6 +1,6 @@
 import argparse
 
-def gen_html(writer, title, repo_owner, repo_name, rdfname, resources):
+def gen_html(writer, title, repo_owner, repo_name, rdfname, resources, version):
     writer.write('<!DOCTYPE html>\n')
     writer.write('<html>\n')
     writer.write(f'<title>{title:s}</title>\n')
@@ -8,8 +8,8 @@ def gen_html(writer, title, repo_owner, repo_name, rdfname, resources):
     writer.write(f'<h1>{title:s}</h1>\n')
     writer.write('<h2>RDF resources</h2>\n')
     writer.write('<ul>\n')
-    writer.write(f'<li><a href="https://{repo_owner:s}.github.io/{repo_name:s}/{rdfname:s}.ttl">https://{repo_owner:s}.github.io/{repo_name:s}/{rdfname:s}.ttl</a></li>\n')
-    writer.write(f'<li><a href="https://{repo_owner:s}.github.io/{repo_name:s}/{rdfname:s}.rdf">https://{repo_owner:s}.github.io/{repo_name:s}/{rdfname:s}.rdf</a></li>\n')
+    writer.write(f'<li><a href="https://{repo_owner:s}.github.io/{repo_name:s}/{version}/{rdfname:s}.ttl">https://{repo_owner:s}.github.io/{repo_name:s}/{version}/{rdfname:s}.ttl</a></li>\n')
+    writer.write(f'<li><a href="https://{repo_owner:s}.github.io/{repo_name:s}/{version}/{rdfname:s}.rdf">https://{repo_owner:s}.github.io/{repo_name:s}/{version}/{rdfname:s}.rdf</a></li>\n')
     writer.write('</ul>\n')
     writer.write('<h2>File resources</h2>\n')
     writer.write('<ul>\n')
@@ -24,6 +24,7 @@ def make_parser():
     parser_.add_argument('-t', '--title', required=True, help='Title for the index page')
     parser_.add_argument('-o', '--owner', required=True, help='Repo owner/group')
     parser_.add_argument('-n', '--name', required=True, help='Repo name')
+    parser_.add_argument('-v', '--version', required=True, help='Ontology file version')
     parser_.add_argument('-r', '--rdfname', required=True, help='Name of the rdf files (without extension)')
     parser_.add_argument('-l', '--resourceslist', required=True, help='List of all resources (with extension)')
     parser_.add_argument('-f', '--outfile', required=True, help='Path of the target html file')
@@ -41,6 +42,7 @@ if __name__ == '__main__':
         "repo_owner": args.owner,
         "repo_name": args.name,
         "rdfname": args.rdfname,
+        "version": args.version,
         "resources": resources_
     }
 
